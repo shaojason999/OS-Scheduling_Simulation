b550678bf193329c6fa6ae56640d7fc9dbb77171
shaojason999 <shaojason999@gmail.com>
2018-12-09 18:35:19 +0800

    First version of scheduler

--- scheduling_simulator.c
+++ scheduling_simulator.c
@@ -2,6 +2,7 @@
 #include <signal.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ucontext.h>
 #include <sys/time.h>
 #include "scheduling_simulator.h"
 
@@ -11,6 +12,8 @@
 #define WAITING 2
 #define TERMINATED 3
 
+/**/int count=0;
+
 struct itimerval old_val, cur_val, new_val;
 int newest_PID;
 struct PID{
@@ -18,9 +21,14 @@ struct PID{
 	int time;	/*0 is small*/
 	int task;	/*task1~task6*/
 	int status;	/*see the #define*/
+int created;
+	ucontext_t *ctx;	/*context*/
+	int stack[1024];	/*assigned to ctx*/
 	struct PID *prev,*next;
 }*PID_inform[TOTAL_PID];
-struct PID *high_queue_cur,*low_queue_cur;	/*the running pid*/
+struct PID *high_queue_cur,*high_queue_prev;	/*the running pid*/
+struct PID *low_queue_cur,*low_queue_prev;	/*the prev-running pid*/
+ucontext_t *new_ctx,*old_ctx;
 
 void hw_suspend(int msec_10)
 {
@@ -39,7 +47,69 @@ int hw_wakeup_taskname(char *task_name)
 
 int hw_task_create(char *task_name)
 {
-    return newest_PID++; // the pid of created task name
+	int temp=task_name[4]-'0';
+	if(temp>6 || temp<1)
+		return -1;
+	return newest_PID++; // the pid of created task name
+}
+void terminate_state()
+{
+	
+}
+void create_a_new_task()
+{
+	high_queue_cur->created=1;
+	getcontext(new_ctx);
+	new_ctx->uc_stack.ss_sp=high_queue_cur->stack;
+	new_ctx->uc_stack.ss_size=sizeof(high_queue_cur->stack);
+//	new_ctx->uc_link=terminate_state;
+	switch(high_queue_cur->task){
+		case 1:	
+			makecontext(new_ctx,task1,0);
+			break;
+		case 2:
+			makecontext(new_ctx,task2,0);
+			break;
+		case 3:
+			makecontext(new_ctx,task3,0);
+			break;
+		case 4:
+			makecontext(new_ctx,task4,0);
+			break;
+		case 5:
+			makecontext(new_ctx,task5,0);
+			break;
+		case 6:
+			makecontext(new_ctx,task6,0);
+			break;
+	}
+}
+void scheduler(int sig_num)
+{
+printf("234\n");
+	if(high_queue_cur!=NULL){
+		high_queue_prev=high_queue_cur;
+		high_queue_cur=high_queue_cur->next;
+		old_ctx=high_queue_prev->ctx;
+		new_ctx=high_queue_cur->ctx;
+		/*creat and set a new task*/
+		if(high_queue_cur->created==0)
+			create_a_new_task();
+		/*(save old and) run new*/
+count++;
+printf("%d\n",count);
+		if(high_queue_cur!=high_queue_prev)
+			swapcontext(old_ctx,new_ctx);
+		else
+			setcontext(new_ctx);
+	}
+
+
+
+
+//	printf("received %d\n",sig_num);
+//	printf("%ld %ld %ld %ld\n",new_val.it_interval.tv_sec,new_val.it_interval.tv_usec,new_val.it_value.tv_sec,new_val.it_value.tv_usec);
+	signal(SIGALRM, scheduler);	/*set again to avoid error*/
 }
 /*add the new task to the prev of the running pid, that is, the end of the ready queue*/
 void add_ready_queue(int pid)
@@ -67,15 +137,6 @@ void add_ready_queue(int pid)
 			low_queue_cur->prev=PID_inform[pid];
 		}
 	}
-}
-void scheduler(int sig_num)
-{
-	
-
-
-//	printf("received %d\n",sig_num);
-//	printf("%ld %ld %ld %ld\n",new_val.it_interval.tv_sec,new_val.it_interval.tv_usec,new_val.it_value.tv_sec,new_val.it_value.tv_usec);
-	signal(SIGALRM, scheduler);	/*set again to avoid error*/
 }
 void input_handler()
 {
@@ -126,14 +187,18 @@ void input_handler()
 			memset(task_name,0,sizeof(task_name));
 			sprintf(task_name,"task%d",task);
 			pid=hw_task_create(task_name);
-			/*pid set*/
-			PID_inform[pid]->priority=priority;
-			PID_inform[pid]->time=time;
-			PID_inform[pid]->task=task;
-			PID_inform[pid]->status=READY;
-			PID_inform[pid]->prev=NULL;
-			PID_inform[pid]->next=NULL;
-			add_ready_queue(pid);
+			if(pid==-1)
+				printf("wrong input, try again\n");
+			else{	/*pid set*/
+				PID_inform[pid]->priority=priority;
+				PID_inform[pid]->time=time;
+				PID_inform[pid]->task=task;
+				PID_inform[pid]->status=READY;
+PID_inform[pid]->created=0;
+				PID_inform[pid]->prev=NULL;
+				PID_inform[pid]->next=NULL;
+				add_ready_queue(pid);
+			}
 		}else if(str[0]=='r'){
 			scanf("%s",str);
 			rmv_pid=atoi(str);
@@ -143,6 +208,9 @@ void input_handler()
 			printf("start\n");
 			signal(SIGTSTP, input_handler);	/*reset the ctrl+z before return*/
 			setitimer(ITIMER_REAL, &old_val, NULL);	/*restore the timer before back to simulation mode*/
+	//		printf("%d %d\n",old_val.it_value.tv_usec);
+//			printf("%d %d\n",old_val.it_interval.tv_usec);
+//	signal(SIGALRM, scheduler);	/*deal the SIGALRM signals with the programmer-defined function*/
 			return;
 		}else
 			printf("wrong input, try again\n");
@@ -163,8 +231,10 @@ void init_variable_set()
 {
 	int i;
 	newest_PID=1;
-	for(i=0;i<TOTAL_PID;++i)
+	for(i=0;i<TOTAL_PID;++i){
 		PID_inform[i]=malloc(sizeof(struct PID));
+		PID_inform[i]->ctx=malloc(sizeof(ucontext_t));
+	}
 	low_queue_cur=NULL;
 	high_queue_cur=NULL;
 }
